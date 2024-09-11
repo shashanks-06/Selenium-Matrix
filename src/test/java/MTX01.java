6c85895e9944e1af3ecc678aca7a3c92f96fb32e
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-09-11 14:20:11 +0530

    Initial Commit

--- src/test/java/MTX01.java
+++ src/test/java/MTX01.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class MTX01 {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MTX01 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.flipkart.com");
+    }
 }
