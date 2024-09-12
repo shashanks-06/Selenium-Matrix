d35baa894f3ed422312074812bf420280a977d3c
shashanks-06 <surjekarshashank@gmail.com>
2024-09-12 21:42:23 +0530

    get, navigate-refresh, back,forward. Thread.sleep()

--- src/test/java/MTX01.java
+++
@@ -1,9 +0,0 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class MTX01 {
-    public static void main(String[] args) {
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.flipkart.com");
-    }
-}
---
+++ src/test/java/Mtx01.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Mtx01 {
+    public static void main(String[] args) throws InterruptedException {
+//        To get the URL
+//        WebDriver webDriver = new ChromeDriver();
+//        webDriver.get("https://www.flipkart.com");
+
+//        To Navigate to the URL
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("https://www.myntra.com");
+        Thread.sleep(2000);
+        driver.navigate().refresh();
+
+        driver.navigate().to("https://www.flipkart.com");
+//        Thread.sleep(2000);
+
+        driver.navigate().to("https://www.amazon.in");
+
+        driver.navigate().back();
+        driver.navigate().forward();
+
+    }
+
+}
