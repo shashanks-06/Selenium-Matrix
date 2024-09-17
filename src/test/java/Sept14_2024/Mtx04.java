c35938c6b96bd7fb81428f2a4357e18b3e8dd037
shashanks-06 <surjekarshashank@gmail.com>
2024-09-17 11:45:31 +0530

    getTitle()

---
+++ src/test/java/Sept14_2024/Mtx04.java
@@ -0,0 +1,23 @@
+package Sept14_2024;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Mtx04 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        String actual = driver.getTitle();
+        System.out.println(actual);
+
+        String expected = "OrangeHRM";
+
+        if (actual.equals(expected)){
+            System.out.println("Title Matched");
+        }else {
+            System.out.println("Title Not Matched");
+        }
+        driver.close();
+    }
+}
