a7f9233537feb9c3d742f62290afe7d3cb53e116
shashanks-06 <surjekarshashank@gmail.com>
2024-09-17 17:20:52 +0530

    CSS SELECTORS - COMBINATION OF TAG NAME

---
+++ src/test/java/Sept16_2024_Locators/Mtx06.java
@@ -0,0 +1,34 @@
+package Sept16_2024_Locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// USING CSS SELECTORS - COMBINATION OF TAG NAME
+public class Mtx06 {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/v1/index.html");
+        driver.manage().window().maximize();
+
+//        USERNAME
+        driver.findElement(By.cssSelector("input[placeholder=\"Username\"]")).sendKeys("standard_user");
+
+//        PASSWORD
+        driver.findElement(By.cssSelector("input[type=\"password\"]")).sendKeys("secret_sauce");
+
+//        LOGIN BUTTON
+        driver.findElement(By.cssSelector("input[value=\"LOGIN\"]")).click();
+
+//        MENU BUTTON
+        driver.findElement(By.cssSelector("div[style=\"z-index: 1000;\"]")).click();
+        Thread.sleep(2000);
+
+//        LOGOUT BUTTON
+driver.findElement(By.cssSelector("a[href=\"./index.html\"]")).click();
+        Thread.sleep(1000);
+
+        driver.close();
+    }
+}
