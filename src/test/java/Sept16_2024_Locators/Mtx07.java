1d1f0de6ea58a95fbadebd0465c0a6691e34ceb4
shashanks-06 <surjekarshashank@gmail.com>
2024-09-17 17:41:16 +0530

    CSS SELECTORS - COMBINATION OF TAG & ID

---
+++ src/test/java/Sept16_2024_Locators/Mtx07.java
@@ -0,0 +1,25 @@
+package Sept16_2024_Locators;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// USING CSS SELECTORS - COMBINATION OF TAG & ID
+public class Mtx07 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/v1/index.html");
+        driver.manage().window().maximize();
+
+//        USERNAME
+        driver.findElement(By.cssSelector("input#user-name")).sendKeys("standard_user");
+
+//        PASSWORD
+        driver.findElement(By.cssSelector("input#password")).sendKeys("secret_sauce");
+
+//        LOGIN BUTTON
+        driver.findElement(By.cssSelector("#login-button")).click();    // We can also write like this without tag name only using the id with "#"
+    }
+}
