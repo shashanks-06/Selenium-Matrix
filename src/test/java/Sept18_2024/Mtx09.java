e4f6bf8c45544e3affa7755067ff63a1e41aae58
shashanks-06 <surjekarshashank@gmail.com>
2024-09-18 20:58:07 +0530

    TAG & ID And TAG & CLASS

---
+++ src/test/java/Sept18_2024/Mtx09.java
@@ -0,0 +1,23 @@
+package Sept18_2024;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Mtx09 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.flipkart.com/");
+        driver.manage().window().maximize();
+
+//        driver.findElement(By.cssSelector("input[name=\"q\"]")).sendKeys("Laptop");
+        // Both Works ->  But [] is mandatory
+//        driver.findElement(By.cssSelector("[name=\"q\"]")).sendKeys("Laptop");
+
+//        TAG , CLASS AND ATTRIBUTE
+//        driver.findElement(By.cssSelector("input.Pke_EE[name=\"q\"]")).sendKeys("Gaming Laptop");
+        // Both Works ->  But [] is mandatory
+        driver.findElement(By.cssSelector(".Pke_EE[name=\"q\"]")).sendKeys("PS5 PRO");
+    }
+
+}
