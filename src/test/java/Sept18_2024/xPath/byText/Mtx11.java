d9e27f0ca419bb93dbe57c1127e10afec9592cc2
shashanks-06 <surjekarshashank@gmail.com>
2024-09-18 21:51:38 +0530

    xPath by using text

--- src/test/java/Sept18_2024/xPath/byText/Mtx11.java
+++ src/test/java/Sept18_2024/xPath/byText/Mtx11.java
@@ -5,12 +5,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Mtx11 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.flipkart.com/");
+//        driver.get("https://www.flipkart.com/");
+//        driver.manage().window().maximize();
+//
+////        xPath by using text
+//        driver.findElement(By.xpath("//span[text()=\"Login\"]")).click();
+
+
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.manage().window().maximize();
 
-//        xPath by using text
-        driver.findElement(By.xpath("//span[text()=\"Login\"]")).click();
+        Thread.sleep(1000); // Website is taking more time to load, so doesn't match the Selenium Speed
+        driver.findElement(By.xpath("//p[text()='Forgot your password? ']")).click();
+
+
     }
 }
