f3b03a4878f729847de48db8b03667195959f0af
shashanks-06 <surjekarshashank@gmail.com>
2024-09-19 22:02:27 +0530

    using indexing to find same attributes values

---
+++ src/test/java/Sept19_2024/Mtx14.java
@@ -0,0 +1,34 @@
+package Sept19_2024;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.SQLOutput;
+
+public class Mtx14 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.flipkart.com");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//input[@name=\"q\"]")).sendKeys("IPhone");
+
+        driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+
+//        using indexing to find same attributes values
+        driver.findElement(By.xpath("(//div[@class=\"KzDlHZ\"])[1]")).click();
+
+        String product = driver.findElement(By.xpath("(//div[@class=\"KzDlHZ\"])[1]")).getText();
+        // To get the text by using .getText();
+        System.out.println(product);
+
+//        xPath by using text
+//        driver.findElement(By.xpath("//span[text()=\"Login\"]")).click();
+
+
+
+
+    }
+}
