e88669deb69afe887a33175afbf60f71ee05c604
shashanks-06 <surjekarshashank@gmail.com>
2024-10-11 22:42:02 +0530

    Writing Excel using Apache POI

---
+++ src/test/java/Part3/Parameterization/Oct11_2024_ExcelWriting/Mtx45_Dynamic.java
@@ -0,0 +1,58 @@
+package Part3.Parameterization.Oct11_2024_ExcelWriting;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Mtx45_Dynamic {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream file;
+        XSSFWorkbook workbook;
+        XSSFSheet sheet;
+
+
+        String PATH = "src/test/java/Part3/Parameterization/TestData/DynamicExcelData.xlsx";
+
+        try {
+            file = new FileOutputStream(PATH);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        workbook = new XSSFWorkbook();
+
+        sheet = workbook.createSheet("Dynamic Data");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Rows: ");
+        int numOfRows = sc.nextInt();
+
+        System.out.print("Enter Columns: ");
+        int numOfCells = sc.nextInt();
+
+        for (int r = 0; r < numOfRows; r++) {
+            XSSFRow currentRow = sheet.createRow(r);
+//            System.out.print(r + " -> ");
+
+            for (int c = 0; c < numOfCells ; c++) {
+                XSSFCell cell = currentRow.createCell(c);
+                cell.setCellValue(sc.next());                   //  next for all types of data types
+//                System.out.print(cell.toString() + " | ");
+            }
+//            System.out.println();
+        }
+
+        workbook.write(file);
+
+        workbook.close();
+        file.close();
+
+        System.out.println("File is Created.........");
+    }
+}
---
+++ src/test/java/Part3/Parameterization/Oct11_2024_ExcelWriting/Mtx46_RandomData.java
@@ -0,0 +1,42 @@
+package Part3.Parameterization.Oct11_2024_ExcelWriting;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Mtx46_RandomData {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream file;
+        XSSFWorkbook workbook;
+        XSSFSheet sheet;
+
+        String PATH = "src/test/java/Part3/Parameterization/TestData/RandomExcelData.xlsx";
+
+        try {
+            file = new FileOutputStream(PATH);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        workbook = new XSSFWorkbook();
+
+        sheet = workbook.createSheet("Single Random Data");
+
+        XSSFRow row = sheet.createRow(3);
+        XSSFCell cell = row.createCell(2);
+        cell.setCellValue("Shashank");
+
+        workbook.write(file);
+
+        workbook.close();
+        file.close();
+
+        System.out.println("File is Created.........");
+
+    }
+}
