20f964f2fd5d6c69f94ed3c8754725e9805659ac
shashanks-06 <surjekarshashank@gmail.com>
2024-10-15 13:57:30 +0530

    Data Driven Testing

--- src/test/java/Part2/DataDrivenTesting/Oct10_2024_ExcelReading/Mtx43_ExcelReading.java
+++ src/test/java/Part2/DataDrivenTesting/Oct10_2024_ExcelReading/Mtx43_ExcelReading.java
@@ -1,4 +1,4 @@
-package Part3.DataDrivenTesting.Oct10_2024_ExcelReading;
+package Part2.DataDrivenTesting.Oct10_2024_ExcelReading;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
--- src/test/java/Part2/DataDrivenTesting/Oct14_2024/Mtx47.java
+++ src/test/java/Part2/DataDrivenTesting/Oct14_2024/Mtx47.java
@@ -1,4 +1,4 @@
-package Part3.DataDrivenTesting.Oct14_2024;
+package Part2.DataDrivenTesting.Oct14_2024;
 
 public class Mtx47 {
     public static void main(String[] args) {
