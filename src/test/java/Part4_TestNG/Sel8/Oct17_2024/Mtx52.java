8fbbc2fd1c90dd406e023ef70368ef14a4c42c9f
shashanks-06 <surjekarshashank@gmail.com>
2024-10-17 22:07:29 +0530

    TestNG Tool Added

---
+++ src/test/java/Part4_TestNG/Sel8/Oct17_2024/Mtx52.java
@@ -0,0 +1,12 @@
+package Part4_TestNG.Sel8.Oct17_2024;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Mtx52 {
+
+    @Test
+    public void test(){     // Always Non-Static Method is used in TestNG
+        Reporter.log("Testing using TestNG", true);
+    }
+}
