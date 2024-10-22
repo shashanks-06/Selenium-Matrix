6d26bd62f7a5d0200b910996feddbfc7a2c22d70
shashanks-06 <surjekarshashank@gmail.com>
2024-10-22 22:05:11 +0530

    Annotations

---
+++ src/test/java/Part4_TestNG/Sel8/Oct22_2024_Annotations/Mtx57_dependsOnMethod.java
@@ -0,0 +1,24 @@
+package Part4_TestNG.Sel8.Oct22_2024_Annotations;
+
+import org.testng.annotations.Test;
+
+public class Mtx57_dependsOnMethod {
+
+    @Test(dependsOnMethods = {"testCase2", "testCase3"})
+    public void testCase1(){
+        System.out.println("Mobile Login Test Case");
+    }
+
+    @Test
+    public void testCase2() throws InterruptedException {
+//        Assert.assertTrue(false);   // To  intentionally fail
+//        Assert.fail();              // To  intentionally fail
+        System.out.println("Web Login Test Case");
+    }
+
+    @Test
+    public void testCase3(){
+//        Assert.fail();          // To  intentionally fail
+        System.out.println("API Login Test Case");
+    }
+}
---
+++ src/test/java/Part4_TestNG/Sel8/Oct22_2024_Annotations/Mtx58_groups.java
@@ -0,0 +1,26 @@
+package Part4_TestNG.Sel8.Oct22_2024_Annotations;
+
+import org.testng.annotations.Test;
+
+public class Mtx58_groups {
+
+    @Test(groups = "Software Company")
+    public void infosys(){
+        System.out.println("Infosys");
+    }
+
+    @Test(groups = "Software Company")
+    public void accenture(){
+        System.out.println("Accenture");
+    }
+
+    @Test(groups = "Automobile Company")
+    public void tata(){
+        System.out.println("Tata");
+    }
+
+    @Test(groups = "Automobile Company")
+    public void mahindra(){
+        System.out.println("Mahindra");
+    }
+}
