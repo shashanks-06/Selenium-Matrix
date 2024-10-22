2aa0324ca7a0e694bbcd4c22014a4ab6ba27dc7f
shashanks-06 <surjekarshashank@gmail.com>
2024-10-22 22:31:00 +0530

    Annotations-Parameters

---
+++ src/test/java/Part4_TestNG/Sel8/Oct22_2024_Annotations/Mtx59_parameters.java
@@ -0,0 +1,31 @@
+package Part4_TestNG.Sel8.Oct22_2024_Annotations;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Mtx59_parameters {
+
+    @Test
+    @Parameters( {"i", "j"} )
+    public void add(int a, int b){
+        System.out.println("Addition : " + (a+b));
+    }
+
+    @Test
+    @Parameters( {"i", "j"} )
+    public void subtract(int a, int b){
+        System.out.println("Subtraction : " + (a-b));
+    }
+
+    @Test
+    @Parameters( {"i", "j"} )
+    public void multiply(int a, int b){
+        System.out.println("Multiplication : " + (a*b));
+    }
+
+    @Test
+    @Parameters( {"i", "j"} )
+    public void divide(int a, int b){
+        System.out.println("Divide : " + (a/b));
+    }
+}
