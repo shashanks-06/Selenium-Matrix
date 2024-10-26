7fd4b252480c476045332b2fad705689bd617537
shashanks-06 <surjekarshashank@gmail.com>
2024-10-26 21:51:55 +0530

    TestNG-ParallelTesting

---
+++ src/test/java/Part4/TestNG/Oct26_ParallelTesting/Mtx63.java
@@ -0,0 +1,20 @@
+package Part4.TestNG.Oct26_ParallelTesting;
+
+import org.testng.annotations.Test;
+
+public class Mtx63 {
+
+    @Test
+    public void method1() throws InterruptedException {
+        System.out.println("This is Method1");
+        Thread.sleep(2000);
+    }
+
+//  Both methods will run at the same time -> parallel -> <parallel="methods" thread-count="2"> in testng.xml
+
+    @Test
+    public void method2() throws InterruptedException {
+        System.out.println("This is Method2");
+        Thread.sleep(2000);
+    }
+}
