d980b1f31e60f72aacd2d3d1024d6353383cfac5
shashanks-06 <surjekarshashank@gmail.com>
2024-11-05 15:52:52 +0530

    Soft Assertions

---
+++ src/test/java/Part4/TestNG/Nov04_2024_Assertions/Mtx68_SoftAssertions.java
@@ -0,0 +1,26 @@
+package Part4.TestNG.Nov04_2024_Assertions;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class Mtx68_SoftAssertions {
+
+    @Test
+    public void TC_Soft(){
+
+        SoftAssert softAssert = new SoftAssert();   //  Soft Assert class object is created
+
+        System.out.println("Step 1");
+        softAssert.assertTrue(false);
+
+        System.out.println("Step 2");
+        softAssert.assertTrue(false);
+
+        System.out.println("Step 3");
+        softAssert.assertTrue(true);
+
+        softAssert.assertAll(); // It is IMPORTANT as even if other assertions fails the test method will still pass
+    }
+}
