901acf2ba99691da2dd64f18ae26bcd119f1e515
shashanks-06 <surjekarshashank@gmail.com>
2024-11-14 14:06:02 +0530

    Excel Reading DDT

--- src/test/java/Part2/DataDrivenTesting/Oct10_2024_ExcelReading/Mtx43_ExcelReading.java
+++ src/test/java/Part2/DataDrivenTesting/Oct10_2024_ExcelReading/Mtx43_ExcelReading.java
@@ -16,7 +16,7 @@ public class Mtx43_ExcelReading {
         XSSFSheet sheet;
 
 
-        String FILE_PATH = "src/test/java/Part3/Parameterization/TestData/MyReadFile.xlsx";
+        String FILE_PATH = "src/test/java/Part2/DataDrivenTesting/TestData/MyReadFile.xlsx";
 
         String rootPath = System.getProperty("user.dir");
         System.out.println(rootPath);
